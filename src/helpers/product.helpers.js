192a788eb8461a1926d8126a6aae7acc2f3bdf7f
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-23 11:41:46 -0300

    feat: helper and request to get catalog

--- src/helpers/product.helpers.js
+++ src/helpers/product.helpers.js
@@ -1,6 +1,20 @@
 import Swal from 'sweetalert2';
 import { getCatalog, getProduct } from '../services/product.services';
 
+export async function viewCatalog() {
+  return getCatalog().then((reply) => {
+    if (reply.sucess) {
+      return reply.catalog;
+    }
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'We were unable to show the catalog',
+      footer: `Server error: ${reply.error}`,
+    });
+    return reply.sucess;
+  });
+}
 export async function viewProduct(id) {
   const token = '';
   const config = {
