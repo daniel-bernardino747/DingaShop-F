91fcfc8a4f3788845597215e0e4f5d6ed9263c34
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-23 18:40:01 -0300

    feat: create api link env and change uri from the services

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export async function postSession(body) {
-  return axios.post('url', body)
+  return axios.post(`http://${process.env.REACT_APP_API}/sign-in`, body)
     .then((token) => ({ token, sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
 
 export async function postUser(body) {
-  return axios.post('url', body)
+  return axios.post(`http://${process.env.REACT_APP_API}/sign-up`, body)
     .then(() => ({ sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
