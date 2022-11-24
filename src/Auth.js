d36666258548bc07d7b76c08dc258166749c48e5
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-24 12:59:51 -0300

    improvement: send reset css to App

--- src/Auth.js
+++ src/Auth.js
@@ -1,18 +1,15 @@
 import PropTypes from 'prop-types';
-import ResetCSS, { AuthContainer } from './assets/global.style';
+import { AuthContainer } from './assets/global.style';
 import lightLogo from './assets/image/logo-lightbg.svg';
 
 export default function Auth({ children }) {
   return (
-    <>
-      <ResetCSS />
-      <AuthContainer>
-        <header>
-          <img src={lightLogo} alt="logo" />
-        </header>
-        {children}
-      </AuthContainer>
-    </>
+    <AuthContainer>
+      <header>
+        <img src={lightLogo} alt="logo" />
+      </header>
+      {children}
+    </AuthContainer>
   );
 }
 Auth.propTypes = {
