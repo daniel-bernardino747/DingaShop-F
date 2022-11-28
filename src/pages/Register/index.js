55387de5252a7db4eb7e1e99698a1cfd43d713e5
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-28 16:24:18 -0300

    feat: remove session to localstorage in RegisterPage

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import AuthForm from '../../components/AuthForm';
 
 import * as s from '../../components/AuthForm/style';
 
 export default function Register() {
+  useEffect(() => {
+    window.localStorage.removeItem('dinga.token');
+    window.localStorage.removeItem('dinga.user');
+  }, []);
+
   return (
     <s.Container>
       <s.Title>Sign-up</s.Title>
