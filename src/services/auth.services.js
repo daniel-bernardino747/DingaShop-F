3fea4ebb67687068005ea12c6c624d3e2743edc4
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-28 17:51:18 -0300

    fix: change http to https

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
 export async function postSession(body) {
-  return axios.post(`http://${process.env.REACT_APP_API}/sign-in`, body)
+  return axios.post(`https://${process.env.REACT_APP_API}/sign-in`, body)
     .then((answer) => ({ data: answer.data, sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
 export async function postUser(body) {
-  return axios.post(`http://${process.env.REACT_APP_API}/sign-up`, body)
+  return axios.post(`https://${process.env.REACT_APP_API}/sign-up`, body)
     .then(() => ({ sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
 export async function renewSession(config) {
-  return axios.put(`http://${process.env.REACT_APP_API}/session`, {}, config)
+  return axios.put(`https://${process.env.REACT_APP_API}/session`, {}, config)
     .then((answer) => ({ data: answer.data, sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
 export async function deleteSession(config) {
-  return axios.delete(`http://${process.env.REACT_APP_API}/session`, config)
+  return axios.delete(`https://${process.env.REACT_APP_API}/session`, config)
     .then(() => ({ sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
 export async function getUserPerfil(config) {
-  return axios.get(`http://${process.env.REACT_APP_API}/user`, config)
+  return axios.get(`https://${process.env.REACT_APP_API}/user`, config)
     .then((user) => ({ data: user.data, sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
--- src/services/product.services.js
+++ src/services/product.services.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export async function getProduct(id) {
-  return axios.get(`http://${process.env.REACT_APP_API}/product/${id}`)
+  return axios.get(`https://${process.env.REACT_APP_API}/product/${id}`)
     .then((answer) => ({ product: answer.data.product, sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
 export async function getCatalog() {
-  return axios.get(`http://${process.env.REACT_APP_API}/products`)
+  return axios.get(`https://${process.env.REACT_APP_API}/products`)
     .then((answer) => ({ catalog: answer.data.catalog, sucess: true }))
     .catch((error) => ({ error, sucess: false }));
 }
